815f67ad7d83d25a9c22d21eec175f0315c7ebbf
Kantun <bekoi10a6@gmail.com>
2022-11-15 18:39:37 +0700

    sửa linh tinh

---
+++ app/src/main/java/laptrinhandroid/fpoly/dnnhm3/Activity/BaoCaoActivity.java
@@ -0,0 +1,16 @@
+package laptrinhandroid.fpoly.dnnhm3.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import laptrinhandroid.fpoly.dnnhm3.R;
+
+public class BaoCaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bao_cao);
+    }
+}
